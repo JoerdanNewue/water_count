82e1b2f5706e4b7dedd136c72bee82eea35b7277
Joerdan Newue <joerdannewue@gmail.com>
2020-07-08 11:08:36 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,5 @@
+N = 2
+kol_vo = float(input("Skol'ko vremeny vie prinimaete dush? (minut)"))
+print("vie prinimaete dush" + " " + str(kol_vo) + " " + "minut!")
+m = float(N * kol_vo)
+print("eto znachit: iz VASHEGO krana Vieteklo " +  str(m) + " " + "litrov vodie!")
